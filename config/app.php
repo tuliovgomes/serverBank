44e60e24434fd21f71eb33a17879353936ee2923
Gabriel Oliveira <gabriel@yampi.com.br>
2020-08-21 11:53:09 -0300

    fix(csfixer) Fix lint error

--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
-        App\Providers\YampiApiServiceProvider::class
+        App\Providers\YampiApiServiceProvider::class,
     ],
 
     /*
