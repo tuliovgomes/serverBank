2199f9e5a84b8fee057b0593a83d21d9eab7862b
Lauro Guedes <leowgweb@gmail.com>
2022-01-07 14:30:45 -0300

    build: update for laravel 8

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('1', function ($api) {
-    $api->group(['prefix' => 'api'], function ($api) {
+    $api->group(['prefix' => 'v1'], function ($api) {
         $api->get('/ping', 'App\Http\Controllers\PingController@ping');
 
         require 'auth.php';
